7cb915a5372c0ab7fa2edb5ba1afb480fd958d59
Joseph Bylund <joseph.bylund@gmail.com>
2013-02-14 17:47:37 -0500

    Finishing self describing numbers.

--- self_describing_numbers.py
+++ self_describing_numbers.py
@@ -1,9 +1,27 @@
 #!/usr/bin/python
 import sys
 
-# determine if inum is happy
+# determine if inum is self describing
 def is_self_describing(inum):
+    pos = 0
+    for pos in range(len(str(inum))):
+        digit = int(str(inum)[pos])
+        # print "digit = " + `digit`
+        occurrences = num_occurrences_of_digit(inum,pos)
+        # print "occurrences of " + `pos` + " = " + `occurrences`
+        if(digit != occurrences):
+            return False
     return True
+
+# count the number of occurrences of digit in
+# the base 10 rep of inum
+def num_occurrences_of_digit(inum, digit):
+    occurrences = 0
+    while(inum > 0):
+        if(digit == inum % 10):
+            occurrences += 1
+        inum //= 10
+    return occurrences
 
 test_cases = open(sys.argv[1], 'r')
 for test in test_cases:
