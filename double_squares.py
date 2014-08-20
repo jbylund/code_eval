3d11cdcf6377cfc6a8fb23344246d71488765582
Joseph Bylund <joe.bylund@moat.com>
2014-08-20 16:18:20 -0400

    (joe) new solution, more better

--- double_squares.py
+++ double_squares.py
@@ -1,46 +1,24 @@
 #!/usr/bin/python
-import sys, math
+import sys
 
-def count_double_sqaures(squares_array,inum):
-    # print "inum  = ", inum
-    i = 0
-    # j = min(len(squares_array)-1,int(math.sqrt(inum)+1))
-    j = len(squares_array) - 1
+def num_of_ways_to_express_as_sum_of_squares(thenum):
+  exprs = 0
+  for i in squares:
+    if i*2 > thenum:
+      continue
+    if (thenum - i) in squares:
+      exprs += 1
+  return exprs
 
-    num_solutions = 0
-    while(i <= j):
-        # if it's too small, then increment the smaller
-        if(squares_array[i] + squares_array[j] < inum):
-            i += 1
-        # if it's too large then decrement the greater
-        elif(squares_array[i] + squares_array[j] > inum):
-            j -= 1
-            if(j < 0):
-                break
-        else: # this is exact equality
-            # print inum, " = ", squares_array[i], " + ", squares_array[j]
-            i += 1
-            j -= 1
-            num_solutions += 1
-            if(j < 0):
-                break
-    return num_solutions
-
-test_cases = open(sys.argv[1], 'r')
-lineslist = list()
-n = 0
-for test in test_cases:
-    if(0 == n):
-        n = int(test)
-    else:
-        lineslist.append(int(test.rstrip()))
-
-perfect_squares = list()
 i = 0
-while(i**2 <= max(lineslist)):
-    perfect_squares.append(i**2)
-    i += 1
+squares = set()
+while i*i <= 2147483647:
+  squares.add(i*i)
+  i += 1
 
-for i in range(len(lineslist)):
-    print count_double_sqaures(perfect_squares,lineslist[i])
+with open(sys.argv[1],'r') as infile:
+  infile.readline()
+  for line in infile:
+    thenum = int(line.strip())
+    print num_of_ways_to_express_as_sum_of_squares(thenum)
 
